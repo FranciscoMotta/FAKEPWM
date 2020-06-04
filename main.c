913ef0adf3adce46d4b8847444acb3889a6b9f47
Julio Francisco Motta <jmottaclaudio@gmail.com>
2020-06-03 23:46:03 -0500

    Definiciones listas

--- main.c
+++ main.c
@@ -11,7 +11,21 @@
 
 #define _XTAL_FREQ 20000000
 
-void limpiaPuertos (void);
+#define LED_PWM LATBbits.LATB0 
+#define UP PORTBbits.RB1
+#define DOWM PORTBbits.RB2
+#define TOPE 100
+
+
+void limpiaPuertos(void);
+
 void main(void) {
+    int counter = 0; 
     return;
+}
+
+void limpiaPuertos(void) {
+    TRISBbits.RB0 = 0; // Ponemos como salida el pin que servirá de pwm
+    TRISBbits.RB1 = 1; // Ponemos en 1 el pin RB1 para que sea entrada 
+    TRISBbits.RB2 = 1; // Ponemos en 1 al pin RB2 para que sea entrada 
 }
